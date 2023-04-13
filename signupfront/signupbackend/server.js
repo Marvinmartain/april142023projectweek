710d0f045ad99da87d405713925062b732558859
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-13 15:27:24 -0400

    router

--- signupfront/signupbackend/server.js
+++ signupfront/signupbackend/server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const routerUrls = require('./routes/routes')
+const routesUrls = require('./routes/routes')
+
 
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, ()=>console.log("Database connected"))
 
+
+app.use('')
 app.listen(4000, ()=> console.log("server is up and running"))
