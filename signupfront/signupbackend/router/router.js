c586d6eeed89a2965e2f57a010f241e27d205466
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-13 15:15:45 -0400

    fullslack

--- signupfront/signupbackend/router/router.js
+++ signupfront/signupbackend/router/router.js
@@ -6,6 +6,15 @@ router.post('/signup', (request, response) => {
     const signedUpUser = new signUpTemplateCopy({
         fullName:request.body.fullName,
         username:request.body.username,
+        email:request.body.email,
+        password:request.body.password
+    })
+    signedUpUser.save()
+    .then(data =>{
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
     })
 })
 
